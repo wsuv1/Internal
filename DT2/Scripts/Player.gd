b85ba1962879dad5f751055e64427900e78d9a04
wsuv1 <wsuvalko@students.mags.school.nz>
2023-06-20 20:17:43 +1200

    Bullet Setup

--- DT2/Scripts/Player.gd
+++ DT2/Scripts/Player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 
 export var speed = 100
+export var Bullet: PackedScene
+onready var Muzzle = $BulletSpawn
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +11,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+# Player Movement
 func _process(delta):
 	var movement_direction = Vector2.ZERO
 	
@@ -26,10 +29,15 @@ func _process(delta):
 
 	look_at(get_global_mouse_position())
 	
+# Calling shoot function
 func _unhandled_input(event): 
 	if event.is_action_released("shoot"):
 		shoot()
 		
-
+# Gun bullet repetition
 func shoot():
-	print("the player has shot!")
+	var bullet_instance = Bullet.instance()
+	add_child(bullet_instance)
+	bullet_instance.global_position = Muzzle.global_position
+	var target = get_global_mouse_position()
+	var direction_to_mouse = bullet_instance.global_position.direction_to(target).normalized 
